1efa7e77916949dbd7bfc044a40c60fd4fc7e5ce
ruben <gutyruben@gmail.com>
2021-10-29 11:28:31 +0200

    tres

---
+++ lote.py
@@ -0,0 +1,119 @@
+from bs4 import BeautifulSoup
+import re
+import requests
+
+
+class Subastas:
+    identificador = list()
+    casasJuzagdo = list()
+    casasExpediente = list()
+    casasEstado = list()
+    casasWeb = list()
+    casasTexto = list()
+    casasHtml = list()
+
+    def __init__(self, subasta, web):
+        self.identificador.append(regexClean(r"<h3>[^<\\h3>]*", subasta)[12:])
+        self.casasJuzagdo.append(regexClean(r"<h4>[^<\\h4>]*", subasta)[4:])
+        self.casasExpediente.append(regexClean(r"Expediente:[^<\\p>]*", subasta)[12:])
+        self.casasEstado.append(regexClean(r"Estado:[^<\\]*", subasta)[8:])
+        self.casasWeb.append(web)
+        self.casasTexto.append(str(subasta.text).replace("\n", "").replace("  ", ""))
+        self.casasHtml.append(str(subasta).replace("\n", "").replace("  ", ""))
+
+
+class SubastaDato:
+    identificador = list()
+    tipoSubasta = list()
+    fechaInicio = list()
+    fechaFin = list()
+    cantidadReclamada = list()
+    lotes = list()
+    formaAdjudicacion = list()
+    anuncioBoe = list()
+    valorSubasta = list()
+    tasacion = list()
+    pujaMinima = list()
+    tramosEntrePujas = list()
+    importeDeposito = list()
+
+    def __init__(self, data):
+        self.identificador.append(regexClean(r"Identificador</th><td><strong>[^<//]*", data)[30:])
+        self.tipoSubasta.append(regexClean(r"Tipo de subasta</th><td><strong>[^<//]*", data)[32:])
+        self.fechaInicio.append(regexClean(r"Fecha de inicio</th><td>[^<//td>]*", data)[24:])
+        self.fechaFin.append(regexClean(r"Fecha de conclusión</th><td><strong class=\"destaca\">[^<//]*", data)[52:])
+        self.cantidadReclamada.append(regexClean(r"Cantidad reclamada</th><td>[^<//]*", data)[27:])
+        self.lotes.append(regexClean(r"Lotes</th><td>[^<\>]*", data)[14:])
+        self.formaAdjudicacion.append(regexClean(r"Forma adjudicación</th><td>[^<//]*", data)[27:])
+        self.anuncioBoe.append(regexClean(r"Anuncio BOE</th><td>[^<\>]*", data)[20:])
+        self.valorSubasta.append(regexClean(r"Valor subasta</th><td>[^<\>]*", data)[22:])
+        self.tasacion.append(regexClean(r"Tasación</th><td>[^<\>]*", data)[21:])
+        self.pujaMinima.append(regexClean(r"Puja mínima</th><td>[^<\>]*", data)[20:])
+        self.tramosEntrePujas.append(regexClean(r"Tramos entre pujas</th><td>[^<\>]*", data)[27:])
+        self.importeDeposito.append(regexClean(r"Importe del depósito</th><td>[^<\>]*", data)[29:])
+
+
+class Lote:
+    identificador = list()
+    valorSubasta = list()
+    importeDeposito = list()
+    pujaMinima = list()
+    tramosEntrePujas = list()
+    descripcion = list()
+    idufir = list()
+    direccion = list()
+    codigoPostal = list()
+    localidad = list()
+    provincia = list()
+    situacionPosesoria = list()
+    visitable = list()
+    inscripcionRegistral = list()
+
+    def __init__(self, data):
+        self.identificador.append(regexClean(r"<div id=\"contenido\"><h2>[^<//]*", data)[32:])
+        self.valorSubasta.append(regexClean(r"Valor Subasta</th><td>[^<//td>]*", data)[22:])
+        self.importeDeposito.append(regexClean(r"Importe del depósito</th><td>[^<//td>]*", data)[29:])
+        self.pujaMinima.append(regexClean(r"Puja mínima</th><td>[^<//td>]*", data)[20:])
+        self.tramosEntrePujas.append(regexClean(r"Tramos entre pujas</th><td>[^<//td>]*", data)[27:])
+        self.descripcion.append(regexClean(r"Descripción</th><td>[^<\>]*", data)[20:])
+        self.idufir.append(regexClean(r"IDUFIR</th><td>[^<\>]*", data)[15:])
+        self.direccion.append(regexClean(r"Dirección</th><td>[^<\>]*", data)[18:])
+        self.codigoPostal.append(regexClean(r"Código Postal</th><td>[^<\>]*", data)[22:])
+        self.localidad.append(regexClean(r"Localidad</th><td>[^<\>]*", data)[18:])
+        self.provincia.append(regexClean(r"Provincia</th><td>[^<\>]*", data)[18:])
+        self.situacionPosesoria.append(regexClean(r"Situación posesoria</th><td>[^<\>]*", data)[28:])
+        self.visitable.append(regexClean(r"Visitable</th><td>[^<\>]*", data)[18:])
+        self.inscripcionRegistral.append(regexClean(r"Inscripción registral</th><td>[^<\>]*", data)[30:])
+
+
+def pagRegex(url):
+    rege = re.search(r"id_busqueda=[^\">]*", str(url))
+    if rege is None:
+        return None
+    else:
+        return rege.group(0).replace("id_busqueda", "https://subastas.boe.es/subastas_ava.php?accion=Mas&id_busqueda")
+
+
+def getUrlSubastas(url):
+    paginas = BeautifulSoup(requests.get(url).content, 'html.parser').find('div', class_='paginar2').find_all('a')
+    data = str(paginas).replace("[", "").replace("]", "").replace(", ", "").split("<a href=\"subastas_ava.php?accion=Mas&amp;")
+    return list(set(filter(None, map(pagRegex, data))))
+
+
+def regexClean(reg, data):
+    rege = re.search(reg, str(data).replace("\n", "").replace("  ", ""))
+    if rege is None:
+        return ''
+    else:
+        return rege.group(0)
+
+
+def regexValues(r):
+    if r is None:
+        return None
+    else:
+        return r.group(0).replace("\n", "").replace("  ", "")
+
+
+def getHtmlSubastas(url):
+    return BeautifulSoup(requests.get(url).content, 'html.parser').find_all('li', class_='resultado-busqueda')
--- main.py
+++ main.py
@@ -3,45 +3,73 @@ import requests
 import pandas as pd
 import re
 
+import lote
+
+
+subastas = lote.Subastas
+subastaDato = lote.SubastaDato
+lotes = lote.Lote
+
+subastaBoeUrl = 'https://subastas.boe.es/'
+#subastaBoeHTML = BeautifulSoup(requests.get(subastaBoeUrl).content, 'html.parser').find_all('area')
+#url2 = lote.regexClean(r"href=\"[^\"]*", lote.regexClean(r"<area alt=\"M.laga[^\>]*", str(subastaBoeHTML))).replace("href=\"", subastaBoeUrl)
+#urlBarcelona = 'https://subastas.boe.es/subastas_ava.php?campo%5B1%5D=SUBASTA.ESTADO&dato%5B1%5D=EJ&campo%5B2%5D=BIEN.TIPO&dato%5B2%5D=I&campo%5B7%5D=BIEN.COD_PROVINCIA&dato%5B7%5D=08&campo%5B16%5D=SUBASTA.FECHA_INICIO_YMD&dato%5B16%5D%5B0%5D=&dato%5B16%5D%5B1%5D=&page_hits=40&sort_field%5B0%5D=SUBASTA.FECHA_FIN_YMD&sort_order%5B0%5D=desc&sort_field%5B1%5D=SUBASTA.FECHA_FIN_YMD&sort_order%5B1%5D=asc&sort_field%5B2%5D=SUBASTA.HORA_FIN&sort_order%5B2%5D=asc&accion=Buscar'
 url = 'https://subastas.boe.es/subastas_ava.php?campo%5B1%5D=SUBASTA.ESTADO&dato%5B1%5D=EJ&campo%5B2%5D=BIEN.TIPO&dato%5B2%5D=I&campo%5B7%5D=BIEN.COD_PROVINCIA&dato%5B7%5D=29&campo%5B16%5D=SUBASTA.FECHA_INICIO_YMD&dato%5B16%5D%5B0%5D=&dato%5B16%5D%5B1%5D=&page_hits=40&sort_field%5B0%5D=SUBASTA.FECHA_FIN_YMD&sort_order%5B0%5D=desc&sort_field%5B1%5D=SUBASTA.FECHA_FIN_YMD&sort_order%5B1%5D=asc&sort_field%5B2%5D=SUBASTA.HORA_FIN&sort_order%5B2%5D=asc&accion=Buscar'
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-
-subastas = soup.find_all('li', class_='resultado-busqueda')
-
-casasHtml = list()
-casasWeb = list()
-casasTexto = list()
-casasTitulo = list()
-casasJuzagdo = list()
-casasExpediente = list()
-casasEstado = list()
-def regexValues(r):
-    if r is None:
-        return None
+urls = lote.getUrlSubastas(url)
+for urlFor in range(len(urls)+1):
+    if urlFor == 0:
+        subastasHTML = lote.getHtmlSubastas(url)
     else:
-        return r.group(0).replace("\n", "").replace("  ", "")
+        subastasHTML = lote.getHtmlSubastas(urls[urlFor-1])
+    for sub in subastasHTML:
+        loteUrl = lote.regexValues(re.search(r"href=\"[^\"]*", str(sub))).replace("href=\".", "https://subastas.boe.es")
+        subastas = lote.Subastas(sub, loteUrl)
+        lotesHTML = BeautifulSoup(requests.get(loteUrl).content, 'html.parser')
+        subastaDato = lote.SubastaDato(lotesHTML)
+        hrmlLotes = re.search(r"href=.*(?=\">Lotes[<\a>]*)", str(lotesHTML.find_all('li')))
+        if hrmlLotes is not None:
+            lotesw = hrmlLotes.group(0).replace("href=\".", "https://subastas.boe.es")
+            numLotes = int(lote.regexClean(r"Lotes</th><td>[^<//td>]*", lotesHTML)[14:])
+            for l in range(numLotes):
+                loteUno = lotesw.replace('&amp;ver=3&amp;idBus=&amp;idLote=&amp;numPagBus=', '&ver=3&idLote={}&idBus=&numPagBus=#cont-tabs'.format(l+1))
+                html = BeautifulSoup(requests.get(loteUno).content, 'html.parser')
+                data = html.find_all('tr')
+                lotes = lote.Lote(html)
 
 
-for i in subastas:
-    web = regexValues(re.search(r"href=\"[^\"]*", str(i))).replace("href=\".", "https://subastas.boe.es")
-    pagew = requests.get(web)
-    soupw = BeautifulSoup(pagew.content, 'html.parser')
-    subastasw = soupw.find_all('tr')
-    lotesw = re.search(r"href=.*(?=\">Lotes[<\a>]*)", str(soupw.find_all('li'))).group(0).replace("href=\".", "https://subastas.boe.es")
+pd.DataFrame({"identificador": subastas.identificador,
+              "juzgado": subastas.casasJuzagdo,
+              "expediente": subastas.casasExpediente,
+              "estado": subastas.casasEstado,
+              "web": subastas.casasWeb,
+              "texto": subastas.casasTexto,
+              "html": subastas.casasHtml}).to_csv('./data/subastas.csv', index=False)
 
-    soupww = BeautifulSoup(requests.get(lotesw).content, 'html.parser')
-    subastasww = soupw.find_all('tr')
+pd.DataFrame({"identificador": subastaDato.identificador,
+              "tipo_subasta": subastaDato.tipoSubasta,
+              "fecha_inicio": subastaDato.fechaInicio,
+              "fecha_fin": subastaDato.fechaFin,
+              "cantidad_reclamada": subastaDato.cantidadReclamada,
+              "lotes": subastaDato.lotes,
+              "forma_Adjudicacion": subastaDato.formaAdjudicacion,
+              "anuncio_BOE": subastaDato.anuncioBoe,
+              "valor_subasta": subastaDato.valorSubasta,
+              "tasacion": subastaDato.tasacion,
+              "puja_minima": subastaDato.pujaMinima,
+              "tramos_entre_pujas": subastaDato.tramosEntrePujas,
+              "importe_deposito": subastaDato.importeDeposito}).to_csv("./data/subastaDato.csv", index=False)
 
-    casasTitulo.append(regexValues(re.search(r"<h3>[^<\\h3>]*", str(i))))
-    casasJuzagdo.append(regexValues(re.search(r"<h4>[^<\\h4>]*", str(i))))
-    casasExpediente.append(regexValues(re.search(r"Expediente:.*[0-9]*/[0-9]*", str(i))))
-    casasEstado.append(regexValues(re.search(r"Estado:.*[^<\\p>]", str(i))))
-    casasWeb.append(web)
-    casasTexto.append(str(i.text).replace("\n", "").replace("  ", ""))
-    casasHtml.append(str(i).replace("\n", "").replace("  ", ""))
-
-df = pd.DataFrame({"titulo": casasTitulo, "juzgado": casasJuzagdo, "expediente": casasExpediente,
-                   "estado": casasEstado, "web": casasWeb, "texto": casasTexto, "html": casasHtml})
-print(df)
-df.to_csv('output.csv', index=False)
+pd.DataFrame({"identificador": lotes.identificador,
+              "valor_subasta": lotes.valorSubasta,
+              "descripcion": lotes.descripcion,
+              "importe_deposito": lotes.importeDeposito,
+              "tramos_entre_pujas": lotes.tramosEntrePujas,
+              "puja_minima": lotes.pujaMinima,
+              "direccion": lotes.direccion,
+              "provincia": lotes.provincia,
+              "localidad": lotes.localidad,
+              "codigo_postal": lotes.codigoPostal,
+              "situacion_posesoria": lotes.situacionPosesoria,
+              "visitable": lotes.visitable,
+              "inscripcion_registral": lotes.inscripcionRegistral,
+              "idufir": lotes.idufir}).to_csv("./data/lotes.csv", index=False)
